6f1756d611836fcd03abd7676594b208150a9f82
Paul Mougin <paul.mougin@gmx.com>
2017-01-16 14:26:05 +0100

    Add Tem images

--- app/src/main/java/com/broz3r/swearism/SwearImage.java
+++ app/src/main/java/com/broz3r/swearism/SwearImage.java
@@ -1,10 +1,19 @@
 package com.broz3r.swearism;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Paul Mougin on 16/01/2017.
  */
 
 public class SwearImage {
+
+    private static SwearImage swearImage1 = new SwearImage(R.drawable.swear_image_1, Side.RIGHT);
+    private static SwearImage swearImage2 = new SwearImage(R.drawable.swear_image_2, Side.LEFT);
+    private static SwearImage swearImage3 = new SwearImage(R.drawable.swear_image_3, Side.LEFT);
+
+    public static List<SwearImage> list = Arrays.asList(swearImage1, swearImage2, swearImage3);
 
     private Side side;
     private int imageId;
